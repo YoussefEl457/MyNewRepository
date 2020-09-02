ec7b586e0cad3c543f6fdaf33a9d5a780217f90e
Youssef <youssefelouamari73@gmail.com>
2020-09-02 16:02:38 +0200

    Dit zijn de oefeningen!

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+//OPDRACHT 1
+//$klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"];
+//print_r($klas);
+
+//OPDRACHT 2
+//$klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"];
+//for($i=0; $i < count($klas); $i++){
+//echo $klas[$i], PHP_EOL;
+//}
+
+//OPDRACHT 3
+//$klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"];
+//foreach($klas as $i){
+//echo $i, PHP_EOL;
+//}
+
+//COMBINATIE OPDRACHT
+//$bucket_list = [];
+//$hoeveelheid_activiteiten = readline("Hoeveel activiteiten wil je hebben in jou bucketlist?");
+//for($i=0; $i < $hoeveelheid_activiteiten; $i++){
+  //$Activity = readline("Wat voor activiteiten wil je hebben in jou bucketlist?");
+  //array_push($bucket_list, $Activity);
+//}
+
+//print_r($bucket_list);
+//foreach ($bucket_list as $x) {
+  //print $x . PHP_EOL;
+//}
+ ?>
